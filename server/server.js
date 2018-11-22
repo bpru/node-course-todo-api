c51f3488611141e1b6defb900abb0a668e0c1ef7
Jen-Chieh Chiang <jchiang36@gatech.edu>
2018-11-22 01:07:25 -0500

    add test case for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.listen(3000, () => {
 app.get('/todos/:id', (req, res) => {
 	let id = req.params.id;
 	if (!ObjectID.isValid(id)) {
-		return res.status(404).send();
+		return res.status(400).send();
 	}
 	Todo.findById(id).then(todo => {
 		if (!todo) {
